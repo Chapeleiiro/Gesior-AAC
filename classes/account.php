7fd8f28de06b0598789425d74a581fbdc52f6902
robertof157 <roberto.f@hotmail.com.br>
2017-09-28 14:25:55 -0300

    Update account.php

--- classes/account.php
+++ classes/account.php
@@ -8,8 +8,8 @@ class Account extends ObjectData
 	const LOADTYPE_NAME = 'name';
 	const LOADTYPE_MAIL = 'email';
 	public static $table = 'accounts';
-	public $data = array('name' => null, 'password' => null, 'premdays' => null, 'coins' => null, 'lastday' => null, 'email' => null, 'key' => null, 'create_ip' => null, 'creation' => null, 'premium_points' => null, 'page_access' => null, 'location' => null, 'rlname' => null, 'birth_date' => null, 'gender' => null, 'email_new' => null, 'email_new_time' => null, 'email_code' => null, 'next_email' => null, 'last_post' => null, 'flag' => null, 'vote' => null, 'loyalty_points' => null, 'guild_points' => null);
-	public static $fields = array('id', 'name', 'password', 'premdays', 'coins', 'lastday', 'email', 'key', 'create_ip', 'creation', 'premium_points', 'page_access', 'location', 'rlname','birth_date', 'gender', 'email_new', 'email_new_time', 'email_code', 'next_email', 'last_post', 'flag', 'vote', 'loyalty_points', 'guild_points');
+	public $data = array('name' => null, 'password' => null, 'premdays' => null, 'coins' => null, 'lastday' => null, 'email' => null, 'key' => null, 'create_ip' => null, 'creation' => null, 'coins' => null, 'page_access' => null, 'location' => null, 'rlname' => null, 'birth_date' => null, 'gender' => null, 'email_new' => null, 'email_new_time' => null, 'email_code' => null, 'next_email' => null, 'last_post' => null, 'flag' => null, 'vote' => null, 'loyalty_points' => null, 'guild_points' => null);
+	public static $fields = array('id', 'name', 'password', 'premdays', 'coins', 'lastday', 'email', 'key', 'create_ip', 'creation', 'coins', 'page_access', 'location', 'rlname','birth_date', 'gender', 'email_new', 'email_new_time', 'email_code', 'next_email', 'last_post', 'flag', 'vote', 'loyalty_points', 'guild_points');
 	public $players;
 	public $playerRanks;
 	public $guildAccess;
@@ -196,7 +196,7 @@ class Account extends ObjectData
 /*
  * Custom AAC fields
  * create_ip , INT, default 0
- * premium_points , INT, default 0
+ * coins , INT, default 0
  * page_access, INT, default 0
  * location, VARCHAR(255), default ''
  * rlname, VARCHAR(255), default ''
